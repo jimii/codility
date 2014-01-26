0eda43e7187b87719b55b3cfb074d6003becdd41
jimi-mac <lukasz@d-7.pl>
2014-01-26 08:44:23 +0100

    Initial Commit

---
+++ PermMissingElem/PermMissingElem/PermMissingElem/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PermMissingElem
+//
+//  Created by jimi on 26.01.2014.
+//  Copyright (c) 2014 jimi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
