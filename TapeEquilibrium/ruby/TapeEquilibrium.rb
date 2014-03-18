6cea6cb44d0333fa9816203ef45531add7cf3e9f
jimi <lukasz@d-7.pl>
2014-03-18 12:00:30 +0100

    cleaner solution

--- TapeEquilibrium/ruby/TapeEquilibrium.rb
+++ TapeEquilibrium/ruby/TapeEquilibrium.rb
@@ -1,30 +1,22 @@
 class TapeEquilibrium
 	def solution(a)
 
-	    right = 0
-	    left = a[0]
-	    ar = Array.new
+		right = 0
+		left = 0
+		result = 1000
+		a.each { |a| right+=a }
+		
+		for i in 0...(a.count-1)
 
-	    for i in 1...a.count
-	        right += a[i]
-	    end
+			left += a[i]
+			right -= a[i]
+			check = (left - right).abs
+			result = check if (check < result)
 
-	    for i in 1...a.count
+		end
 
-	        check = (left - right).abs
-	        ar[i-1] = check
-	        left += a[i]
-	        right -= a[i]
-
-	    end
-
-	    find = ar.min
-
-	    if a.count == 2
-	        find = (a[0]-a[1]).abs
-	    end
-
-	    find
-
+		result = (a[0]-a[1]).abs if a.count==2
+		result
+		
 	end
 end
