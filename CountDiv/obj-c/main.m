dcb7d4b9713ffe1a3ebd32aa17cdfec234f0a6e2
jimii <lukasz.golebiowski@icloud.com>
2014-09-30 19:05:01 +0200

    obj-c solution

---
+++ CountDiv/obj-c/main.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  CountDiv
+//
+//  Created by jimi on 30.09.2014.
+//  Copyright (c) 2014 jimi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        unsigned long int A = 6;
+        unsigned long int B = 11;
+        unsigned long int K = 2;  
+
+        unsigned long int result = 0;
+
+        if (A%K == 0) {
+        	result = ((B-A)/K)+1;
+        } else {
+        	result = (B/K - ((A/K)+1))+1;
+        }
+
+        return (int)result;
+        
+    }
+    return 0;
+}
--- CountDiv/ruby/CountDiv.rb
+++ CountDiv/ruby/CountDiv.rb
@@ -2,15 +2,15 @@ class CountDiv
 
 	def solution(a, b, k)
 
-    result = 0
+		result = 0
 
-    if a%k==0
-      result = ((b-a)/k)+1
-    else
-      result = (b/k - ((a/k)+1))+1
-    end
-    
-    result
+		if a%k==0
+			result = ((b-a)/k)+1
+		else
+			result = (b/k - ((a/k)+1))+1
+		end
+		
+		result
 
 	end
 
